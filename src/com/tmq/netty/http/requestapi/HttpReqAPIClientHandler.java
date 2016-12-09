0c44f1503da3a442be06ffde0687f33195160b6f
quytm <quytm_58@vnu.edu.vn>
2016-12-09 15:28:48 +0700

    delete comment

--- src/com/tmq/netty/http/requestapi/HttpReqAPIClientHandler.java
+++ src/com/tmq/netty/http/requestapi/HttpReqAPIClientHandler.java
@@ -15,43 +15,6 @@ public class HttpReqAPIClientHandler extends SimpleChannelInboundHandler<HttpObj
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, HttpObject msg) {
-//        if (msg instanceof HttpResponse) {
-//            HttpResponse response = (HttpResponse) msg;
-//
-//            System.err.println("STATUS: " + response.status());
-//            System.err.println("VERSION: " + response.protocolVersion());
-//
-//            if (!response.headers().isEmpty()) {
-//                for (CharSequence name : response.headers().names()) {
-//                    for (CharSequence value : response.headers().getAll(name)) {
-//                        System.err.println("HEADER: " + name + " = " + value);
-//                    }
-//                }
-//            }
-//
-//            if (response.status().code() == 200 && HttpUtil.isTransferEncodingChunked(response)) {
-//                readingChunks = true;
-//                System.err.println("CHUNKED CONTENT {");
-//            } else {
-//                System.err.println("CONTENT {");
-//            }
-//        }
-//        if (msg instanceof HttpContent) {
-//            HttpContent chunk = (HttpContent) msg;
-//            System.err.println(chunk.content().toString(CharsetUtil.UTF_8));
-//
-//            if (chunk instanceof LastHttpContent) {
-//                if (readingChunks) {
-//                    System.err.println("} END OF CHUNKED CONTENT");
-//                } else {
-//                    System.err.println("} END OF CONTENT");
-//                }
-//                readingChunks = false;
-//            } else {
-//                System.err.println(chunk.content().toString(CharsetUtil.UTF_8));
-//            }
-//        }
-
         if (msg instanceof HttpContent) {
             HttpContent chunk = (HttpContent) msg;
             responseString.append(chunk.content().toString(CharsetUtil.UTF_8));
